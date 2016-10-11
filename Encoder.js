c7e37c2e521a23ee9ef7f0c722f4379bef321dc6
max sami <skull037037@gmail.com>
2016-10-11 08:52:57 -0700

    Oct 11 8:52
    changed background color, added caesar cipher object, and checks which
    radio is pressed down

--- Encoder.js
+++ Encoder.js
@@ -1,23 +1,56 @@
+//Caesar cipher converter object
+var CaesarCipher= {
+  "a":"z",
+  "b":"a",
+  "c":"b",
+  "d":"c",
+  "e":"d",
+  "f":"e",
+  "g":"f",
+  "h":"g",
+  "i":"h",
+  "j":"i",
+  "k":"j",
+  "l":"k",
+  "m":"l",
+  "n":"m",
+  "o":"n",
+  "p":"o",
+  "q":"p",
+  "r":"q",
+  "s":"t",
+  "t":"s",
+  "u":"t",
+  "v":"u",
+  "w":"v",
+  "x":"w",
+  "y":"x",
+  "z":"y"
+}
+
 //event Handler
-$("input[type='radio']").mousedown(function(a) {
+$("input[type='radio']").click(function(a) {
 $("input:checked").prop('checked',false);
 $(this).prop('checked',true);
+checkClicked();
 });
 
-$("#inputArea").keydown(function(e){
-       $("textArea").append(String.fromCharCode(event.which));
-  });
-
-//echos input if echo radio is pressed down
+//checks which radio is active
+function checkClicked(){
 if($("input:checked").val() == "echo"){
-  console.log("echo");
-
+  //echos input if echo radio is pressed down
+  $("#inputArea").keydown(function(e){
+         $("#textArea").append(String.fromCharCode(event.which));
+    });
 }
-if($("input:checked").val() == "Heiroglyphics"){
+else if($("input:checked").val() == "Heiroglyphics"){
   console.log("Heiroglyphics");
 
 }
-if($("input:checked").val() == "Caesar cipher"){
-  console.log("Caesar cipher");
+else if($("input:checked").val() == "Caesar cipher"){
+  $("#inputArea").keydown(function(e){
+         $("#textArea").append("A");
+    });
 
+}
 }
