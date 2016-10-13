a2836fdc04e8a62fc1988bc7224944a527dad76f
max sami <skull037037@gmail.com>
2016-10-13 08:43:11 -0700

    Oct 13 8:42
    added runes and changed background color back to white to avoid image
    issues

--- Encoder.js
+++ Encoder.js
@@ -94,6 +94,36 @@ var HeiroglyphicCipher = {
   "":"",
   " ":" "
 }
+var RuneCipher = {
+  "A": "<img src='images/Runes/a.gif'>",
+  "B": "<img src='images/Runes/b.gif'>",
+  "C":"<img src='images/Runes/c.gif'>",
+  "D": "<img src='images/Runes/d.gif'>",
+  "E":"<img src='images/Runes/e.gif'>",
+  "F":"<img src='images/Runes/f.gif'>",
+  "G":"<img src='images/Runes/g.gif'>",
+  "H":"<img src='images/Runes/h.gif'>",
+  "I":"<img src='images/Runes/i.gif'>",
+  "J":"<img src='images/Runes/j.gif'>",
+  "K":"<img src='images/Runes/k.gif'>",
+  "L":"<img src='images/Runes/l.gif'>",
+  "M":"<img src='images/Runes/m.gif'>",
+  "N":"<img src='images/Runes/n.gif'>",
+  "O":"<img src='images/Runes/o.gif'>",
+  "P":"<img src='images/Runes/p.gif'>",
+  "Q":"<img src='images/Runes/q.gif'>",
+  "R":"<img src='images/Runes/r.gif'>",
+  "S":"<img src='images/Runes/s.gif'>",
+  "T":"<img src='images/Runes/t.gif'>",
+  "U":"<img src='images/Runes/u.gif'>",
+  "V":"<img src='images/Runes/v.gif'>",
+  "W":"<img src='images/Runes/w.gif'>",
+  "X":"<img src='images/Runes/x.gif'>",
+  "Y":"<img src='images/Runes/y.gif'>",
+  "Z":"<img src='images/Runes/z.gif'>",
+  "":"",
+  " ":" "
+}
 //event Handler
 $("input[type='radio']").click(function() {
 $("input:checked").prop('checked',false);
@@ -116,6 +146,15 @@ else if($("input:checked").val() == "Heiroglyphics"){
     });
 
 }
+else if($("input:checked").val() == "Runes"){
+  $("#inputArea").keydown(function(){
+    var MessageToEncode = String.fromCharCode(event.which);
+    var Encoding = MessageToEncode.split('');
+    MessageToEncode = RuneCipher[Encoding[0]];
+             $("#textArea").append( MessageToEncode );
+    });
+
+}
 //translates message into the cipher if the radio is true
 else if($("input:checked").val() == "Caesar cipher"){
   $("#inputArea").keydown(function(){
@@ -128,7 +167,7 @@ MessageToEncode = CaesarCipher[Encoding[0]];
 
   }
   else{
-    console.log("error");
+    console.log("fatal error");
   }
 });
 $("#translationInputArea").keydown(function(){
